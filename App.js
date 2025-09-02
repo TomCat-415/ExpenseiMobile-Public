4300faa947c5e6f8c253607af36ded4845305273
TomCat415 <thomas.clinard@gmail.com>
2025-09-02 16:26:56 +0900

    add placeholder App.js for language detrction

---
+++ App.js
@@ -0,0 +1,7 @@
+// Placeholder file to indicate React Native/JavaScript project
+import { Text } from 'react-native';
+
+export default function App() {
+  return <Text>Expensei Placeholder</Text>;
+}
+
